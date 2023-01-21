df33aac7f0dd14d3590432b9fa8424bd4c8e444e
Azizbek Asadov <azizbek1998@icloud.com>
2023-01-21 14:42:05 +0500

    Initial Commit

---
+++ ContainerPattern_SwiftUI/ContainerPattern_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContainerPattern_SwiftUIApp.swift
+//  ContainerPattern_SwiftUI
+//
+//  Created by Azizbek Asadov on 21/01/23.
+//
+
+import SwiftUI
+
+@main
+struct ContainerPattern_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
